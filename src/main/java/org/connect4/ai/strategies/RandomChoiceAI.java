294585ba417c88d5b29620bbaff76bdb9e42365f
Hassan Tarek <hassanelashry415@gmail.com>
2023-11-30 09:05:11 +0200

    Finish RandomChoice AI implementation

--- src/main/java/org/connect4/ai/strategies/RandomChoiceAI.java
+++ src/main/java/org/connect4/ai/strategies/RandomChoiceAI.java
@@ -3,16 +3,26 @@ package org.connect4.ai.strategies;
 import org.connect4.ai.enums.AIType;
 import org.connect4.game.core.Board;
 
+import java.util.Random;
+
 public class RandomChoiceAI extends AI {
     private final Board board;
+    private final Random random;
 
     public RandomChoiceAI(Board board) {
         super(AIType.RANDOM_CHOICE_AI);
         this.board = board;
+        this.random = new Random();
     }
 
     @Override
     public int getNextMove() {
-        return 0;
+        int col;
+
+        do {
+            col = random.nextInt(Board.COLS) + 1;
+        } while (!board.isValidMove(col));
+
+        return col;
     }
 }
