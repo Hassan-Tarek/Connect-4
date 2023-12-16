5aa9fa3b7f9fe5d0f69f8213143a74fa8a6cb7b3
Hassan Tarek <hassanelashry415@gmail.com>
2023-12-16 19:02:40 +0200

    Implemented the client side

--- src/main/java/org/connect4/networking/client/Client.java
+++ src/main/java/org/connect4/networking/client/Client.java
@@ -1,5 +1,7 @@
 package org.connect4.networking.client;
 
+import java.util.Scanner;
+
 public class Client {
     private static final String SERVER_ADDRESS = "localhost";
     private static final int PORT = 4444;
@@ -7,5 +9,22 @@ public class Client {
     public static void main(String[] args) {
         ClientManager clientManager = new ClientManager(SERVER_ADDRESS, PORT);
         clientManager.connectToServer();
+
+        Scanner scanner = new Scanner(System.in);
+        Thread sendThread = new Thread(() -> {
+            while (true) {
+                String message = scanner.nextLine();
+                clientManager.sendMessage(message);
+            }
+        });
+        sendThread.start();
+
+        Thread receiveThread = new Thread(() -> {
+            while (true) {
+                String message = (String) clientManager.getReceivedMessage();
+                System.out.println("Message: " + message);
+            }
+        });
+        receiveThread.start();
     }
 }
--- src/main/java/org/connect4/networking/client/ClientManager.java
+++ src/main/java/org/connect4/networking/client/ClientManager.java
@@ -1,8 +1,16 @@
 package org.connect4.networking.client;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class ClientManager {
     private final String serverAddress;
     private final int port;
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
 
     public ClientManager(String serverAddress, int port) {
         this.serverAddress = serverAddress;
@@ -10,6 +18,39 @@ public class ClientManager {
     }
 
     public void connectToServer() {
+        try {
+            socket = new Socket(serverAddress, port);
+            in = new ObjectInputStream(socket.getInputStream());
+            out = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public Object getReceivedMessage() {
+        try {
+            return in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendMessage(Object message) {
+        try {
+            out.writeObject(message);
+            out.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void close() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
